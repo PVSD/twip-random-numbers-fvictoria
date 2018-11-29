13d15286d67a4abfadbca0b3c38ccfb4b65825b3
fvictoria <felipevictoria630@yahoo.com>
2018-11-29 11:20:11 -0500

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
-
+import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Random rndm = new Random();
+        int count = 0;
+        for(int i = 1;i < 1000;i++)
+        {
+            int a = 1+rndm.nextInt(100);
+            System.out.println(a+" ");
+            count++;
+            if(count > 10)
+            {
+                System.out.println(" ");
+                count = 0;
+
+            }
+            
+        }
     }
 }
