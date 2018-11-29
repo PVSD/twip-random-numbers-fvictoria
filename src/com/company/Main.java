bbeedf3247bcf9a3a0a220ed1acc70a78e8937db
fvictoria <41639211+fvictoria@users.noreply.github.com>
2018-11-29 11:15:49 -0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,19 +3,23 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-	Random rndm = new Random();
-	int a = 0; int count = 0;
-	for(int i = 1;i < 100;i++)
-	{
-	int a = 1+rndm.nextInt(100);
-	System.out.println(a+" ");
-	count++;
-	if(count > 10)
-	{
-	System.out.println(" ");
-	count = 0;	
-	}
-		
-	}
+        Random rndm = new Random();
+        int count = 0;
+        for(int i = 1;i < 1000;i++)
+        {
+            int a = 1+rndm.nextInt(100);
+            System.out.println(a+" ");
+            count++;
+            if(count > 10)
+            {
+                System.out.println(" ");
+                count = 0;
+
+            }
+            
+
+
+
+        }
     }
 }
