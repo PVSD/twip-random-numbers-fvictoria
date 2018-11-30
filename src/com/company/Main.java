9baac0413d1153a32a6935ca825ec915f7df0f60
fvictoria <felipevictoria630@yahoo.com>
2018-11-29 22:55:49 -0500

    not 100% right

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,24 @@
 package com.company;
 import java.util.Random;
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
         Random rndm = new Random();
         int count = 0;
-        for(int i = 1;i < 1000;i++)
+        int total = 0;
+        Scanner kbReader = new Scanner(System.in);
+        System.out.println("Would you like to run the loop 10x, 100x, or 1000x?");
+        int loopNum = kbReader.nextInt();
+        for(int i = 1;i <=loopNum ;i++)
         {
             int a = 1+rndm.nextInt(100);
-            System.out.println(a+" ");
+            System.out.println(a);
             count++;
-            if(count > 10)
-            {
-                System.out.println(" ");
-                count = 0;
-
-            }
-
-
-
+            total+= count;
 
         }
+        int Avg = total/loopNum;
+        System.out.println("\n"+"Average:"+Avg);
     }
 }
